5215d1d3fed037f78fb766b72e04179a0b71bc90
Rafael Vieira <32908327+vieirarafael8@users.noreply.github.com>
2019-02-13 23:41:34 +0000

    Login created

--- app/src/main/java/com/example/cenas/MainActivity.java
+++ app/src/main/java/com/example/cenas/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.cenas;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static int TIME_OUT = 3000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OUT);
     }
-}
+    }
+
